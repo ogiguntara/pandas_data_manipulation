a4d54f7c7a88d1fd0aa78a64ac3c39d4d40742b9
ogiguntara <ogiguntara@gmail.com>
2022-06-26 07:35:20 +0700

    inspecting values

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
 #!python3
 
+from defusedxml import DTDForbidden
 import pandas as pd
 
 if __name__ == "__main__":
+    df_homelessness = pd.read_csv("./data/homelessness.csv")
+    
+    #inspecting values
+    print(df_homelessness.head())
+    print(df_homelessness.values)
+    print(df_homelessness.columns)
+    print(df_homelessness.index)
     
